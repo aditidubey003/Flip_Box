a21a8d0978b44d31c2cdc192fce76e83161dae8d
Aditi Dubey <aditidubey576@gmail.com>
2023-05-19 22:51:34 +0530

    completed the functionality

--- code/script.js
+++ code/script.js
@@ -1,30 +1,34 @@
-/*let box = document.querySelector('.box')
-
-box.addEventListener("click", function (){
-        let allBoxes = document.querySelectorAll('.box')
-        allBoxes.forEach((box) => {
-         box.innerText = getNumber()
-        })
-})
-
-
-function getNumber(){
-  let number = Math.floor(Math.random()* 12)
-  console.log(number);
-  return number;
-}
-*/
 
 
 
-let allBoxes = document.querySelectorAll('.box')
+// First Box Without Event Delegation
 
-allBoxes.forEach((box) => {
+let bigBox1 = document.querySelectorAll('.box-1 div')
+
+bigBox1.forEach((box, index) => {
   box.addEventListener("click", function (event){
-   event.target.innerText = Math.floor(Math.random()* 12);
+   event.target.innerText = index + 1;
 
  setTimeout(()=> {
-       event.target.innerText = "";     
+  event.target.innerText = "";     
  }, 3000)
-        })
+  })
 })
+
+// Second Box With Event Delegation
+
+
+let bigBox2 = document.querySelector('.box-2')
+
+bigBox2.addEventListener("click", function (event){
+  let text = event.target.dataset.text 
+console.log(text)
+event.target.innerText = text;
+
+setTimeout(()=> {
+  event.target.innerText = "";     
+ }, 3000)
+
+})
+
+
