52d04c29c1196513aa83605700f422d1e675a827
Aditi Dubey <aditidubey576@gmail.com>
2023-05-19 12:31:49 +0530

    worked on functionality

--- code/script.js
+++ code/script.js
@@ -1,1 +1,30 @@
+/*let box = document.querySelector('.box')
 
+box.addEventListener("click", function (){
+        let allBoxes = document.querySelectorAll('.box')
+        allBoxes.forEach((box) => {
+         box.innerText = getNumber()
+        })
+})
+
+
+function getNumber(){
+  let number = Math.floor(Math.random()* 12)
+  console.log(number);
+  return number;
+}
+*/
+
+
+
+let allBoxes = document.querySelectorAll('.box')
+
+allBoxes.forEach((box) => {
+  box.addEventListener("click", function (event){
+   event.target.innerText = Math.floor(Math.random()* 12);
+
+ setTimeout(()=> {
+       event.target.innerText = "";     
+ }, 3000)
+        })
+})
